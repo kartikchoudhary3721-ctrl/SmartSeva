142ebaec5cd202f7e4840e4acf9bf1c8f7182353
kartikchoudhary3721-ctrl <kartikchoudhary3721@gmail.com>
2026-02-12 14:08:34 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -48,6 +48,10 @@ res.json({reply:"Server Error"});
 
 });
 
-app.listen(3000,()=>{
-console.log("Server running http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+console.log("Server running on port " + PORT);
 });
+
+
